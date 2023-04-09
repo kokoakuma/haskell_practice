37d7aad9e6ec6de4bad20b2ded666402df0a09aa
kokoakuma <aikw9101@gmail.com>
2023-04-09 08:09:29 +0000

    use type

--- baby.hs
+++ baby.hs
@@ -9,3 +9,12 @@ doubleSmallNumber x = if x > 100
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 
 triples = [(a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], c^2 == a^2 + b^2, c + a + b == 24 ]
+
+removeNonUppercase:: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
